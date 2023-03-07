527b442076a78dc9e4e9133af7bcc5c48a892e4e
estelajank <51270593+estelajank@users.noreply.github.com>
2023-03-07 17:14:15 +0000

    Update streamlit_app.py
    changing fruit table's index

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,7 @@ streamlit.header('🍌🍊 Build your own Fruit Smootie 🥝🍇')
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #adding a pick list so a person can choose fruits they want to include
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
