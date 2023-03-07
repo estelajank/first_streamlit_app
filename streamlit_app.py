47d23bf4889a48196a675b7b51cbec561ac58b4d
estelajank <51270593+estelajank@users.noreply.github.com>
2023-03-07 17:12:53 +0000

    Update streamlit_app.py
    adding a selector for fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,4 +12,10 @@ streamlit.header('🍌🍊 Build your own Fruit Smootie 🥝🍇')
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+#adding a pick list so a person can choose fruits they want to include
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+#display the table on the page
 streamlit.dataframe(my_fruit_list)
+
